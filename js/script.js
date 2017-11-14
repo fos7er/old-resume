a3fe0b028b9ac6cd9b7941c6344cef4a2c2560b8
stozen-spb <2012stalker@gmail.com>
2017-11-14 17:54:52 +0300

    added starter scren disable if user was on site last 10 minutes

--- js/script.js
+++ js/script.js
@@ -11,6 +11,57 @@ starterMainScreen.classList.add("animation-dissappear");
 
 });
 
+// если пользователь был на сайте за последние 10 минут, то отключаем стартовый экран
+
+var coockieOptions = {
+  expires: 600 
+};
+
+if ( getCookie('wasLast10minutes') === 'true' ) {
+  var starterScreen = document.querySelector('.starter-screen-container');
+  starterScreen.style.display = 'none';
+
+}
+
+setCookie('wasLast10minutes', true , coockieOptions);
+
+function setCookie(name, value, options) {
+  options = options || {};
+
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+// возвращает cookie с именем name, если есть, если нет, то undefined
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 $(document).on('click', 'a.smoothscroll', function(event){
     event.preventDefault();
 
